93e39038df9cd13815c711ff5f92fead704dd268
cli3338198 <cli3338198@gmail.com>
2024-07-15 13:40:23 -0700

    added guards to routes

--- api/views.py
+++ api/views.py
@@ -10,18 +10,29 @@ def recipe_idea(request):
   query = request.GET.get('q', 'chicken')
   url = 'https://api.edamam.com/search'
   params = {
-        'q': query,
-        'app_id': settings.EDAMAM_RECIPE_API_ID,
-        'app_key': settings.EDAMAM_RECIPE_API_KEY
-    }
-  response = requests.get(url, params=params)
-  data = response.json()
+    'q': query,
+    'app_id': settings.EDAMAM_RECIPE_API_ID,
+    'app_key': settings.EDAMAM_RECIPE_API_KEY
+  }
+  try:
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    data = response.json()
+  except requests.RequestException as e:
+    return JsonResponse({'error': str(e)}, status=500)
 
   recipes = data.get('hits')
+
+  if not recipes:
+    return JsonResponse({'message': 'No recipes found'}, status=404)
+
   recipe = recipes[0]
+
+  print(recipe['recipe'])
+
   label = recipe['recipe'].get('label')
   image = recipe['recipe'].get('image')
-  ingredients = [ing['food'] for ing in recipe['recipe'].get('ingredients')]
+  ingredients = [ing['food'] for ing in recipe['recipe'].get('ingredients', [])]
 
   filtered_data = {
     'label': label,
@@ -35,12 +46,16 @@ def ingredient_info(request):
   ingredient = request.GET.get('ingredient', 'apple')
   url = 'https://api.edamam.com/api/nutrition-data'
   params = {
-      'ingr': ingredient,
-      'app_id': settings.EDAMAM_NUTRITION_API_ID,
-      'app_key': settings.EDAMAM_NUTRITION_API_KEY
+    'ingr': ingredient,
+    'app_id': settings.EDAMAM_NUTRITION_API_ID,
+    'app_key': settings.EDAMAM_NUTRITION_API_KEY
   }
-  response = requests.get(url, params=params)
-  data = response.json()
+  try:
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    data = response.json()
+  except requests.RequestException as e:
+    return JsonResponse({'error': str(e)}, status=500)
 
   calories = data.get('calories')
   health_labels = data.get('healthLabels')
